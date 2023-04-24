28aa1066b50704413048ccb3462fa9d1a4e22e20
Han Ye Htun <sloth30799@gmail.com>
2023-04-25 00:35:40 +0630

    fix: tsconfig

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,3 +1,4 @@
+import * as path from "path"
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
@@ -6,6 +7,9 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     extensions: [".tsx", ".ts", ".jsx", ".js"],
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
   },
   server: {
     port: 3000,
