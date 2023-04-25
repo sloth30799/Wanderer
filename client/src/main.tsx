5476c8379eb574592e09694e32a1eb66cb3197d2
Han Ye Htun <sloth30799@gmail.com>
2023-04-25 13:21:35 +0630

    fix(\): tsconfig error

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,14 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
+import { Provider } from "react-redux"
 import { StyledEngineProvider, ThemeProvider } from "@mui/material/styles"
 import { CssBaseline } from "@mui/material"
 import { AnimatePresence } from "framer-motion"
-import { Provider } from "react-redux"
-import { store } from "./services/store.js"
-import { theme } from "./theme"
-import "./assets/main.css"
-import App from "./App.jsx"
 import { Toaster } from "react-hot-toast"
+import App from "App.js"
+import "assets/main.css"
+import { theme } from "theme"
+import { store } from "services/store"
 
 const rootElement = document.getElementById("root")
 if (rootElement !== null) {
