b858651eb3a3037deccfa377b37d3417f4310441
Han Ye Htun <sloth30799@gmail.com>
2023-04-25 18:37:06 +0630

    fix: fix tsconfig

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,10 +5,6 @@ import tsconfigPaths from "vite-tsconfig-paths"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), splitVendorChunkPlugin()],
-  resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js"],
-    alias: [{ find: "@", replacement: "/src" }],
-  },
   server: {
     port: 3000,
     proxy: {
