3671f4ebefe69802553bd15d9c2914815d6b131a
Han Ye Htun <sloth30799@gmail.com>
2023-04-25 14:17:38 +0630

    fix: ts error

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from "vite"
+import { defineConfig, splitVendorChunkPlugin } from "vite"
 import react from "@vitejs/plugin-react"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), splitVendorChunkPlugin()],
   resolve: {
     extensions: [".tsx", ".ts", ".jsx", ".js"],
-    // alias: [{ find: "@", replacement: "/src" }],
+    alias: [{ find: "@", replacement: "/src" }],
   },
   server: {
     port: 3000,
