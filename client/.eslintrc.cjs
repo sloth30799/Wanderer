3c4cf3a6206369837809be4c1807641cf610b392
Han Ye Htun <sloth30799@gmail.com>
2023-04-26 10:11:48 +0630

    fix: tsconfig

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -4,13 +4,11 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
-    "prettier",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   plugins: ["react-refresh"],
-  rules: {},
-  global: {
-    module: "writable",
+  rules: {
+    "react-refresh/only-export-components": "warn",
   },
 }
