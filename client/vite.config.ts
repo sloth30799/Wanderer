60329caa153c45c96dbe2b5b5e98934b335a7668
Han Ye Htun <sloth30799@gmail.com>
2023-04-26 10:52:07 +0630

    fix: build error

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,9 @@
-import { defineConfig, splitVendorChunkPlugin } from "vite"
+import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), splitVendorChunkPlugin()],
+  plugins: [react()],
   server: {
     port: 3000,
     proxy: {
